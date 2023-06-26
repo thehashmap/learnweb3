c6c3833a22f06fcd422ee86c487f3d258cc7dc35
thehashmap <nkmnikhil0009@gmail.com>
2023-06-26 16:00:23 +0530

    deploy

--- dex/hardhat/scripts/deploy.js
+++ dex/hardhat/scripts/deploy.js
@@ -1,28 +1,36 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Deploy the Token Contract
+  const tokenContract = await hre.ethers.deployContract("Token");
+  await tokenContract.waitForDeployment();
+  console.log("Token deployed to:", tokenContract.target);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // Deploy the Exchange Contract
+  const exchangeContract = await hre.ethers.deployContract("Exchange", [
+    tokenContract.target,
+  ]);
+  await exchangeContract.waitForDeployment();
+  console.log("Exchange deployed to:", exchangeContract.target);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  // Wait for 30 seconds to let Etherscan catch up on contract deployments
+  await sleep(30 * 1000);
+
+  // Verify the contracts on Etherscan
+  await hre.run("verify:verify", {
+    address: tokenContract.target,
+    constructorArguments: [],
+    contract: "contracts/Token.sol:Token",
   });
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await hre.run("verify:verify", {
+    address: exchangeContract.target,
+    constructorArguments: [tokenContract.target],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
