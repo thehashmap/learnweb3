75d8da3127e3a00e205caf2be02624554a354f8b
thehashmap <nkmnikhil0009@gmail.com>
2023-07-05 00:36:22 +0530

    switch network

--- lottery/hardhat.config.js
+++ lottery/hardhat.config.js
@@ -3,19 +3,19 @@ require("dotenv").config({ path: ".env" });
 
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    mumbai: {
+    sepolia: {
       url: QUICKNODE_HTTP_URL,
       accounts: [PRIVATE_KEY],
     },
   },
   etherscan: {
     apiKey: {
-      polygonMumbai: POLYGONSCAN_KEY,
+      sepolia: ETHERSCAN_API_KEY,
     },
   },
 };
